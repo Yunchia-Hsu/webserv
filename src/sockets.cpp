e7b79847cf2fdd297c2a9819687ad587b949ba40
Yunchia-Hsu <yh42hsu@gmail.com>
2025-02-11 16:54:31 +0200

    test

--- src/sockets.cpp
+++ src/sockets.cpp
@@ -1,5 +1,9 @@
 
-// UDP Socket 的流程，主要就 socket() -> 填位址 -> sendto()/recvfrom()
-//TCP Socker connect() / listen() / accept()。
+#include <iostream>
 
-int socketfd = socket(AF_INET, SOCK_DGRAM, 0);// ipv4
+
+int main (int argc, char *argv[])
+{
+
+	return 0;
+}
