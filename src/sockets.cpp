7da4a55951c04a2273cd81a812fb45bf52c741dd
Yunchia-Hsu <yh42hsu@gmail.com>
2025-02-11 16:53:08 +0200

    added main in hsu

--- src/sockets.cpp
+++ src/sockets.cpp
@@ -1,5 +1,28 @@
 
+#include <arpa/inet.h> //socket(), bind(), accept(), inetntop(), listen() 
+#include <iostream>
+#include <stdio.h>
+
 // UDP Socket 的流程，主要就 socket() -> 填位址 -> sendto()/recvfrom()
 //TCP Socker connect() / listen() / accept()。
 
-int socketfd = socket(AF_INET, SOCK_DGRAM, 0);// ipv4
+
+
+
+
+
+
+int main (int argc, char ** argv)
+{
+
+	//create sockets
+	int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);// (ipv4  domain, type of the service, protocol)
+	if (socket_fd <= 0)
+		perror("socket created error");
+
+
+
+
+
+	return 0; 
+}
